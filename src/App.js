35e6dbc47251abd3e13dbe1fc0a87b3e8fd77304
Elijahscriptdev <elijahobominuru@gmail.com>
2021-05-12 07:37:02 +0100

    clean up files

--- src/App.js
+++ src/App.js
@@ -12,34 +12,3 @@ function App() {
 }
 
 export default App;
-
-// import Button from "@material-ui/core/Button";
-
-// import Cards from "./components/Cards";
-// import Grid from "@material-ui/core/Grid";
-// import { makeStyles } from '@material-ui/core/styles';
-
-// const classes = useStyles();
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-// }));
-
-// <div className=''>
-//   <h1>Weather Exercise - React-JS </h1>
-//   <Button variant='contained' color='primary' href='#contained-buttons'>
-//     Link
-//   </Button>
-//   <div className={classes.root}>
-//     <Grid container spacing={3}>
-//       <Grid item xs={12}>
-//         <Cards />
-//       </Grid>
-//       <Grid item xs={12}>
-//         <Cards />
-//       </Grid>
-//     </Grid>
-//   </div>
-// </div>
